a4654ac8c94bbe693a11d5ae2d84b39b16b95205
Michele Angrisano <michele.angrisano@gmail.com>
2026-07-20 15:07:40 +0200

    jpick 1.5.0

--- Formula/jpick.rb
+++ Formula/jpick.rb
@@ -1,8 +1,8 @@
 class Jpick < Formula
   desc "Tiny jq-like JSON tool written in C++20"
   homepage "https://github.com/mangrisano/jpick"
-  url "https://github.com/mangrisano/jpick/archive/refs/tags/v1.4.0.tar.gz"
-  sha256 "f5b090f320045005e8f3aad032fe914d4232c8d024d2d13db45a6ff56609bdbe"
+  url "https://github.com/mangrisano/jpick/archive/refs/tags/v1.5.0.tar.gz"
+  sha256 "9283a511c1439db5eaaa524bbddb330c68d3c0724065a3d23c32fced6207839b"
   license "MIT"
   head "https://github.com/mangrisano/jpick.git", branch: "main"
 
